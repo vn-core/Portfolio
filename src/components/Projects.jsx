4c6225c0d680b4da35fcbaef68fd4f81916c3f5a
Vanessa Contreras <vanessa.ncore@gmail.com>
2025-08-28 21:19:44 -0400

    Actualizacion de dominio de marketplace

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ const Projects = () => {
               </div>
               <div className="project-links">
                 <a 
-                  href="https://marketplace-trends.netlify.app" 
+                  href="https://trendsmarketplacecl.netlify.app" 
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="project-link"
